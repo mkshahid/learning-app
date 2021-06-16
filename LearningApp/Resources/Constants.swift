9093095d2273350bb2224da83ad12de9e86e0b03
Michael <mkshahid@usc.edu>
2021-06-16 12:46:13 -0700

    - add content detail view - add constants

---
+++ LearningApp/Resources/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  LearningApp
+//
+//  Created by Michael Shahidi on 6/16/21.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static var videoHostUrl = "https://codewithchris.github.io/learningJSON/"
+}
